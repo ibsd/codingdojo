2f50b3526c3b3f8c00e1705d8cfb72d6f2ba15da
ibsd <icebergbsd@gmail.com>
2019-04-18 10:54:33 +0800

    Init Commit

---
+++ Args/test/app/AppTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAssertTrue() {
+        assertTrue(true);
+    }
+}
